b75098c58dea4def00f71adc51a47b24f69d6ba5
lilawalker <lilawlker@gmail.com>
2020-10-30 13:13:49 +0000

    Update seeds with correct product colour combinations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,36 @@
 products = [
-  ["mens", "classic", "/images/classic_black.jpg", "black"],
-  ["mens", "classic", "/images/classic_gold.jpg", "gold"],
-  ["mens", "classic", "/images/classic_white.jpg", "white"],
-  ["mens", "high top", "/images/hightop_black.jpg", "black"],
-  ["mens", "high top", "/images/hightop_gold.jpg", "gold"],
-  ["mens", "high top", "/images/hightop_white.jpg", "white"],
-  ["mens", "old school", "/images/oldschool_black.jpg", "black"],
-  ["mens", "old school", "/images/oldschool_gold.jpg", "gold"],
-  ["mens", "old school", "/images/oldschool_white.jpg", "white"],
-  ["womens", "classic", "/images/classic_black.jpg", "black"],
-  ["womens", "classic", "/images/classic_gold.jpg", "gold"],
-  ["womens", "classic", "/images/classic_white.jpg", "white"],
-  ["womens", "high top", "/images/hightop_black.jpg", "black"],
-  ["womens", "high top", "/images/hightop_gold.jpg", "gold"],
-  ["womens", "high top", "/images/hightop_white.jpg", "white"],
-  ["womens", "old school", "/images/oldschool_black.jpg", "black"],
-  ["womens", "old school", "/images/oldschool_gold.jpg", "gold"],
-  ["womens", "old school", "/images/oldschool_white.jpg", "white"],
+  ["mens", "classic", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["mens", "classic", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["mens", "classic", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["mens", "high top", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["mens", "high top", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["mens", "high top", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["mens", "old school", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["mens", "old school", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["mens", "old school", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["womens", "classic", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["womens", "classic", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["womens", "classic", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["womens", "high top", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["womens", "high top", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["womens", "high top", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["womens", "old school", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["womens", "old school", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
+  ["womens", "old school", Faker::Lorem.sentence(word_count: 3), Faker::Lorem.paragraph(sentence_count: 4)],
 ]
 
-products.each do |category, style, image, colour|
-  3.times do
+products.each do |category, style, name, description|
+  colours = ["black", "white", "gold"]
+
+  colours.each do |colour|
+    image = "/images/#{style.underscore}_#{colour}.jpg"
     product = Product.create(
       category: category,
       style: style,
       image: image,
       colour: colour,
-      name: Faker::Lorem.sentence(word_count: 3),
-      description: Faker::Lorem.paragraph(sentence_count: 4),
+      name: name,
+      description: description,
       price: Faker::Commerce.price(range: 60..129),
     )
 
