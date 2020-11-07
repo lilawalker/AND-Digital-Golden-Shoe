79aac05fedb72079e6f32ff28eb3d2a17350b7f9
lilawalker <lilawlker@gmail.com>
2020-11-07 20:00:08 +0000

    Add test for feature to sign in before checkout

--- spec/features/order_feature_spec.rb
+++ spec/features/order_feature_spec.rb
@@ -11,4 +11,12 @@ RSpec.feature 'Order', type: :feature do
     expect(page).not_to have_button "Checkout"
     expect(page).to have_content "Your basket is empty"
   end
+
+  scenario "user must be logged in to checkout" do
+    visit "/products/#{unit.product.id}?size=6"
+    click_button "Add to basket"
+    page.find(:css, 'a[href="/basket"]').click
+    click_link 'Checkout'
+    expect(page).to have_button "Log in"
+  end
 end
