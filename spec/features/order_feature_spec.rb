0e7ee585b9857265a99435cc255c092be34e3d7b
lilawalker <lilawlker@gmail.com>
2020-11-07 20:11:53 +0000

    Add feature to confirm order on checkout and display order details

--- spec/features/order_feature_spec.rb
+++ spec/features/order_feature_spec.rb
@@ -19,4 +19,27 @@ RSpec.feature 'Order', type: :feature do
     click_link 'Checkout'
     expect(page).to have_button "Log in"
   end
+
+  context "logged in" do
+    before do
+      visit '/users/sign_in'
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Log in'
+      visit "/products/#{unit.product.id}?size=6"
+      click_button "Add to basket"
+      page.find(:css, 'a[href="/basket"]').click
+      click_button "Checkout"
+    end
+
+    scenario "order can be checked out and confirmed if signed in" do
+      expect(page).to have_content "Order placed"
+      expect(page).to have_content "Order Number: #{user.orders.first.id}"
+    end
+
+    scenario "basket is empty after checkout" do
+      page.find(:css, 'a[href="/basket"]').click
+      expect(page).to have_content "Your basket is empty"
+    end
+  end
 end
